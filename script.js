94b8643280cb0b8745507d12931e7fda21a82582
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-10-21 11:50:13 -0300

    🎉 Commit inicial

---
+++ script.js
@@ -0,0 +1,41 @@
+// console.log e uma função nativa do js, que nos permite exibir uma mensagem no cosole
+
+// console e uma ferramenta do navegador
+console.log("Olá Mundo!")
+console.log(3 * 5)
+
+
+//temos 3 tipos variaveis
+
+// var aluno = "vinicius" <-- Não utilizamos mais a Variável do tipo (var)
+
+//Variável com let podem ser reatribuídas
+let aluno = "vinicius"; //string = texto, quando tem as apas("")
+
+// Variável const não podem ser reatribuídas
+const cpf = 888888888;// number = numeros
+
+// boolean = true ou false
+let nome = false; //true
+console.log(nome)
+// null = nulo ou inexistente
+let vini = null;
+console.log(vini)
+//undefined = ainda não definido
+let dia;
+console.log(dia)
+
+// dia = 20
+// console.log(dia)
+
+// NaN = não é um número
+console.log(5 / "y")
+
+// tipos de dados
+
+// string = textos
+// number = numeros
+// boolean = true ou false (let diaLindo = true)
+// null = nulo ou inexistente (let vini = null;)
+// NaN = não é um número (console.log(5 / "y");)
+// underfind = ainda não definido (let escola;  console.log(escola);)
